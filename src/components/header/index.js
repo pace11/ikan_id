9aeb293e0e0f4ac36acc96c80c63f3452b3d8fd6
pace11 <ryanjoker87@gmail.com>
2021-03-07 14:05:19 +0700

    feat: add component header and styling write component header, add styling to use in global component

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="uikit-header">
+      <div className="img-logo">
+        <img
+          src="https://www.efishery.com/uploads/images/logo/4e250933d7f7fe68a5c16700b61a4cfc.png"
+          alt="logo-app"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Header
